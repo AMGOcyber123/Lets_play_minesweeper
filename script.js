186dbbd9c55641387741dc6a3f46b2364df8db03
AMGOcyber123 <aayumodi123@gmail.com>
2022-11-07 11:23:55 +0530

    new functions included in script file

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 // HTML file has to run on a server so as to avoid CORS error
-import { SetBoard } from "./board.js"
+import { SetBoard, markTile } from "./board.js"
 
 //console.log(SetBoard(2,2));
-const BOARD_SIZE = 4;
-const NO_MINES = 2;
-
+let BOARD_SIZE = 10;
+let NO_MINES = 10;
+let counter = 10;
 const board = SetBoard(BOARD_SIZE,NO_MINES);
 const boardElement = document.querySelector(".board")
+const mineLeftText =  document.querySelector("[data-mine-count]"); 
 console.log(board)
-boardElement.style.setProperty("--size",BOARD_SIZE)
+
 board.forEach(row => {
     row.forEach(tile => {
-         boardElement.append(tile.elements)
+        boardElement.append(tile.element)
+        tile.element.addEventListener('click',()=>{})
+        tile.element.addEventListener('contextmenu',e =>{
+            e.preventDefault();
+            markTile(tile);
+        })
     });
 });
+
+boardElement.style.setProperty("--size",BOARD_SIZE)
+mineLeftText.textContent = NO_MINES
